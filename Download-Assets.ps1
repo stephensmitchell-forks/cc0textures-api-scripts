eba9abd94fdd930720cd6caf06e0366ed93d3868
StruffelProductions <31403260+StruffelProductions@users.noreply.github.com>
2020-06-27 13:27:26 +0200

    New parameters + formatting
    Added parameters for filetype and attribute, changed formatting of the download log

--- Download-Assets.ps1
+++ Download-Assets.ps1
@@ -4,12 +4,16 @@ Param(
     [String]$query,
     [ValidateSet("PhotoTexturePBR","PhotoTexturePlain","SBSAR","3DModel")][String]$type,
     [ValidateSet("Alphabet","Popular","Latest")][String]$sort,
-    [String]$id
+    [String]$id,
+    [String]$attribute,
+    [String]$filetype,
+    [Boolean]$makeSubfolders=$true
 )
 
 ###FUNCTIONS###
 
-function FormatSize($bytes)  #Slightly modified version of https://stackoverflow.com/a/40887001
+#Slightly modified version of https://stackoverflow.com/a/40887001
+function FormatSize($bytes)
 {
     $suffix = "B", "KB", "MB", "GB", "TB"
     $index = 0
@@ -25,11 +29,10 @@ function FormatSize($bytes)  #Slightly modified version of https://stackoverflow
 ###MAIN SCRIPT###
 
 #Initialize variables for the rest of the script
-#TODO Expose the Regexes as parameters
 
 $apiUrl = "https://cc0textures.com/api/v1/downloads_csv"
-$attributeRegex = [RegEx]("")
-$filetypeRegex = [RegEx]("")
+$attributeRegex = [RegEx]("$attribute")
+$filetypeRegex = [RegEx]("$filetype")
 $downloadDirectory = "$PSScriptRoot\Downloads"
 
 $getParameters = @{
@@ -42,7 +45,7 @@ $getParameters = @{
 #Run the webrequest
 
 $webRequest = Invoke-WebRequest -Uri "$apiUrl" -Body $getParameters
-$apiOutput = ($webRequest.Content | ConvertFrom-Csv)
+$apiOutput = ($webRequest.Content | ConvertFrom-Csv | Where-Object{ $_.DownloadAttribute -match $attributeRegex -and $_.Filetype -match $filetypeRegex})
 
 #Display the number of results and ask user whether to continue
 #TODO Display more info (such as total size)
@@ -51,17 +54,24 @@ $apiOutput = ($webRequest.Content | ConvertFrom-Csv)
 $numberOfDownloads = $apiOutput.Count
 $totalSizeBytes = ($apiOutput | Measure-Object -Property Size -Sum).Sum
 $totalSizeFormatted = FormatSize($totalSizeBytes)
+$downloadedSizeBytes=0
 
 write-host "Found $numberOfDownloads files with a total size of $totalSizeFormatted " -f green
 pause
 
 #Loop over API output and perform downloads
 
-$apiOutput | Where-Object{ $_.DownloadAttribute -match $attributeRegex -and $_.Filetype -match $filetypeRegex } | ForEach-Object{
+
+$apiOutput | ForEach-Object{
 
     #Define output directory and final filename
 
-    $destinationDirectory = Join-Path -Path $downloadDirectory -ChildPath $_.AssetID
+    if($makeSubfolders){
+        $destinationDirectory = Join-Path -Path $downloadDirectory -ChildPath $_.AssetID
+    }else{
+        $destinationDirectory = $downloadDirectory
+    }
+    
     $destinationFile = Join-Path -Path $destinationDirectory -ChildPath ("{0}_{1}.{2}" -f $_.AssetID,$_.DownloadAttribute,$_.Filetype)
     $sourceUrl = if($_.PrettyDownloadLink -eq ''){$_.RawDownloadLink}else{$_.PrettyDownloadLink}
 
@@ -72,8 +82,13 @@ $apiOutput | Where-Object{ $_.DownloadAttribute -match $attributeRegex -and $_.F
         write-host "Created directory: $destinationDirectory"
     }
 
-    #Display what's being downloaded and start the download
+    #Calculate progression in percent
 
-    "{0}/{1} ({2} Bytes)" -f $_.AssetID,$_.DownloadAttribute,$_.Size
+    $percentCompleted = (($downloadedSizeBytes / $totalSizeBytes) * 100).ToString("00.0")
+
+    #Display what's being downloaded and start the download
+    "[{0}%] {1}_{2} ({3})" -f $percentCompleted,$_.AssetID,$_.DownloadAttribute,(FormatSize($_.Size))
     Start-BitsTransfer -Source $sourceUrl -Destination $destinationFile
+    $downloadedSizeBytes = $downloadedSizeBytes + $_.Size
 }
+Write-Host "Completed."
