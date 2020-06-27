636ce03db2df11fb74949fcedcd1793d2b4d16f3
StruffelProductions <31403260+StruffelProductions@users.noreply.github.com>
2020-06-27 20:21:57 +0200

    Declare intended input

--- Create-Credentials.ps1
+++ Create-Credentials.ps1
@@ -1,1 +1,4 @@
+write-host "Enter your KeyID and your key in the pop-up."
+pause
 Get-Credential | Export-CliXml -Path "$PSScriptRoot\Patreon-Credentials.xml"
+write-host "Your credentials were saved in $PSScriptRoot\Patreon-Credentials.xml"
